f3cf05bf317ca743d8fef23d08b01d1469d11f3a
mobina-cpu <mobbinate00@gmail.com>
2024-10-16 14:02:50 +0330

    create state,city,zone models

--- address/models.py
+++ address/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+    state = models.ForeignKey(
+        State, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Zone(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+
