f3009e1806e2bb7db5af76fc18b4aab9b85ac0a1
mobina-cpu <mobbinate00@gmail.com>
2024-10-19 22:13:28 +0330

    add corsheaders to installed_app

--- storefront/settings.py
+++ storefront/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "debug_toolbar",
     'rest_framework',
+    'corsheaders',
     'product',
     'customer',
     'address'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS =[
+    'http://localhost:3000'
+]
