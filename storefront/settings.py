2dbb4c3725f3ba872e6c9a5e39660d6bc97d7f62
mobina-cpu <mobbinate00@gmail.com>
2024-10-19 19:38:08 +0330

    add debug_toolbar, rest_framework to installed_apps

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
+    'rest_framework',
     'product',
     'customer',
     'address'
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'storefront.urls'
