fcdae278535e334775d6e1fa501be2c2644bb986
mobina-cpu <mobbinate00@gmail.com>
2024-10-20 00:50:19 +0330

    all urls fore views

---
+++ product/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:category_id>/', views.ProductsByCategoryAPIView.as_view()),
+]
