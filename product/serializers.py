8cfdf90708dc79b1795589309164d53c040dbbbe
mobina-cpu <mobbinate00@gmail.com>
2024-10-20 00:49:39 +0330

    create serializer

--- product/serializers.py
+++ product/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from product.models import Category
+from .models import (
+    Category,
+    Product,
+    ProductAttribute,
+    AttributeGroup
+)
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -24,3 +29,52 @@ class CategorySerializer(serializers.ModelSerializer):
         if children.exists():
             return CategorySerializer(children, many=True).data
         return None
+
+
+class AttributeGroupSerializer(serializers.ModelSerializer):
+    attributes = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = AttributeGroup
+        fields = [
+            'id',
+            'title',
+            'attributes'
+        ]
+
+
+class ProductAttributeSerializer(serializers.ModelSerializer):
+    attribute = serializers.StringRelatedField()
+
+    class Meta:
+        model = ProductAttribute
+        fields = [
+            'id',
+            'attribute',
+            'value',
+        ]
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField(allow_null=True)
+    attribute_group = serializers.StringRelatedField(many=True)
+    attributes = ProductAttributeSerializer(many=True)
+    images = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'name',
+            'description',
+            'inventory',
+            'status',
+            'category',
+            'brand',
+            'attribute_group',
+            'attributes',
+            'images'
+
+        ]
