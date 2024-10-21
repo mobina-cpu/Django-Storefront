c40753c6193be1de29f4270adac3eef07da892b0
mobina-cpu <mobbinate00@gmail.com>
2024-10-21 22:49:43 +0330

    add brand endpoints

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #category endpoint
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
-    path('categories/<int:category_id>/', views.CategoryProductsListView.as_view()),
+    path('categories/<int:category_id>/products/', views.CategoryProductsListView.as_view()),
+
+    #brand endpoint
+    path('brands/', views.BrandListView.as_view()),
+    path('brands/<int:brand_id>/products/', views.BrandProductsListView.as_view()),
 
 ]
