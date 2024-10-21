d3319ff7495ea3ba48a86d7ebc52aecdb572e3e0
mobina-cpu <mobbinate00@gmail.com>
2024-10-21 22:51:32 +0330

    cleanup

--- product/urls.py
+++ product/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #category endpoint
+    #category endpoints
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('categories/<int:category_id>/products/', views.CategoryProductsListView.as_view()),
 
-    #brand endpoint
+    #brand endpoints
     path('brands/', views.BrandListView.as_view()),
     path('brands/<int:brand_id>/products/', views.BrandProductsListView.as_view()),
 
