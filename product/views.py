e39365b38d351c4e6ac059ad9e33e22a89a57b6d
mobina-cpu <mobbinate00@gmail.com>
2024-10-21 21:02:02 +0330

    cleanup

--- product/views.py
+++ product/views.py
@@ -1,39 +1,54 @@
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import (
+    status,
+    generics
+)
 
-from .models import Category, Product
+from .models import (
+    Category,
+    Product
+)
 from .serializers import (
     CategorySerializer,
     ProductSerializer
 )
+from .pagination import DefaultPageNumberPagination
 
 
-class CategoryList(APIView):
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.filter(parent=None)
+    serializer_class = CategorySerializer
+    pagination_class = DefaultPageNumberPagination
+
     @staticmethod
-    def get(request):
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
+    def post(request):
+        if request.user.is_staff:
+            serializer = CategorySerializer(data=request.data)
 
-        return Response(serializer.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+            return Response(
+                {"detail": "You do not have permission to perform this action."}, status=status.HTTP_403_FORBIDDEN
+            )
 
 
-class ProductsByCategory(APIView):
+class CategoryProductsListView(generics.ListAPIView):
     serializer_class = ProductSerializer
 
-    @staticmethod
-    def get(request, category_id):
-        try :
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        try:
             category = Category.objects.get(id=category_id)
-
         except Category.DoesNotExist:
-            return Response(
-                {"detail": "Category does not exist"}, status=status.HTTP_404_NOT_FOUND
-            )
-        products = Product.objects.filter(category=category)
-        serializer = ProductSerializer(products, many=True)
+            raise NotFound(detail="Category not found.")
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Product.objects.filter(category=category)
+
 
 
 
